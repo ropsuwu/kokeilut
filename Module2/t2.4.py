6c267a127f15e09189697103665bfcd026f843c6
roope <roopejrhavu@gmail.com>
2025-08-25 13:09:51 +0300

    tehtäviä 3. moduulista

--- Module2/t2.4.py
+++ Module2/t2.4.py
@@ -3,5 +3,5 @@ lk2 = float(input("Anna toinen luku, kiitos! : "))
 lk3 = float(input("Anna kolmas luku NYT! :"))
 summa = (lk1 + lk2 + lk3)
 tulo = (lk1 * lk2 * lk3)
-keksiarvo = ((lk1 + lk2 + lk3)/3)
+keksiarvo = summa/3
 print("Tässä on summa, tulo ja keskiarvosi!",summa, tulo, keksiarvo)
--- Module4/assignment-2.py
+++
@@ -1,4 +0,0 @@
-# Your solution for Part 2, Assignment 2
-# Ratkaisusi Osa 2, tehtävään 2
-
-print("Your solution...")
