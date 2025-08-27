7d2f041aaac339a43e5b7be9f62a17e2b90e9198
roope <roopejrhavu@gmail.com>
2025-08-27 11:54:30 +0300

    tuntiteht.

--- tuntitehtavat/olympialaiset.py
+++ tuntitehtavat/olympialaiset.py
@@ -1,5 +1,5 @@
 vuosi = int(input("Anna vuosi: "))
-if (vuosi == 2020 or vuosi == 1916 or vuosi == 1940 or vuosi == 1944):
+if vuosi == 2020 or vuosi == 1916 or vuosi == 1940 or vuosi == 1944:
     print("Ei ollut olympiavuosi.")
 elif vuosi == 2021:
     print("On olympiavuosi.")
